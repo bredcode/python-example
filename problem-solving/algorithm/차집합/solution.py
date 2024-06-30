92cbe0cace2abf6fadab2846d1cdb740f2667774
kkw.ko <kkw.ko@samsung.com>
2024-06-30 21:09:02 +0900

    차집합 문제 및 풀이 생성

---
+++ problem-solving/algorithm/차집합/solution.py
@@ -0,0 +1,11 @@
+nA, nB = map(int, input().split())
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+result = sorted(A.difference(B))
+
+if len(result) == 0:
+  print(0)
+else:
+  print(len(result))
+  print(result)
